eb6479f891ccbb0ada57bd4a3416d2bca21913f1
Awad Mackie <firesock.serwalek@gmail.com>
2012-12-07 23:04:15 +0000

    Move image tag beneath chat and add more appropriate form submission

--- client/js/client.js
+++ client/js/client.js
@@ -4,21 +4,6 @@ socket.on('join', function (data) {
 	$('#channel_box').text('Name: ' + data.name + '\n');
 });
 
-
-var miliseconds = 2000;
-$(document).keypress(function(e) {
-	if (e.which == 13) {
-		socket.emit('say', {msg: $('#text_input').val()});
-		$('#text_input').val('');
-		$('#text_input').attr('disabled', 'true');
-		$('#text_input').val('WAIT FOR ' + miliseconds);
-		setTimeout(function() {
-			$('#text_input').val('');
-			$('#text_input').removeAttr('disabled');
-		}, miliseconds);
-	}
-});
-
 socket.on('msg', function(data) {
 	$('#channel_box').append(data.name + ': ' + data.msg + '\n');
 	$('#channel_box').scrollTop($('#channel_box')[0].scrollHeight);
@@ -26,7 +11,20 @@ socket.on('msg', function(data) {
 	$('img#img_area').attr('src', data.msg);
 });
 
-
 $(document).ready(function() {
-	$('#text_input').focus();
+	var miliseconds = 2000;
+	$('form#chat').submit(function(e) {
+		e.preventDefault();
+		
+		socket.emit('say', {msg: $('#text_input').val()});
+		$('#text_input').val('');
+		$('#text_input').attr('disabled', 'disabled');
+		$('#text_input').val('WAIT FOR ' + miliseconds);
+		setTimeout(function() {
+			$('#text_input').val('');
+			$('#text_input').removeAttr('disabled');
+			$('#text_input').focus();
+		}, miliseconds);
+
+	});
 });
